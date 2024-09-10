eb4c516e2737c08bc5810edca40adf16b0846b05
Seiju-N <seiju.nomura@gmail.com>
2024-09-10 18:17:51 +0900

    b

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ pool.connect()
   .then(() => console.log('Postgres connected'))
   .catch(err => console.error('Postgres connection error', err));
 
-console.log(DATABASE_URL);
+console.log(process.env.DATABASE_URL);
 const saveBannedUser = async (userData, reason) => {
   const queryText = `
         INSERT INTO banned_users (
